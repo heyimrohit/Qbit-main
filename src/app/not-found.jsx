84852a4e727cc2e05fcdc7ee4c5f599455889372
heyimrohit <rohituaike81@gmail.com>
2025-04-09 17:18:22 +0530

    rect : 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,19 @@
-import Link from 'next/link';
-
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
-    <div className="container flex-col gap-7 justify-center items-center py-12">
-      <div className="flex-col gap-2 justify-center items-center">
-        <span className="md:text-6xl text-xl font-bold">That Page Cant Be Found</span>
-        <span className="text-center text-sm md:text-lg">It looks like nothing was found at this location. Maybe try to
-          search for what you are looking for?</span>
-      </div>
-      <div>
-        <Link href="/" className="bg-black text-white p-3 rounded-md hover:bg-gray-800">Go back to Home</Link>
-      </div>
+    <div className="flex flex-col gap-5 items-center justify-center py-20">
+      <h1 className="text-4xl font-bold">
+        That Page Cant Be Found
+      </h1>
+      <p className="mt-4 text-center max-w-md">
+        It looks like nothing was found at this location. Maybe try to search
+        for what you are looking for?
+      </p>
+      <a
+        href="/"
+        className="bg-black text-white p-3 rounded-md hover:bg-gray-800"
+      >
+        Go back to Home
+      </a>
     </div>
-  )
+  );
 }
